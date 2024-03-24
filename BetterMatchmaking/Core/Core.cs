6610c0c726cdbdbcb207e458b7e07dc0188424e4
GreenComfyTea <klunics97@gmail.com>
2024-03-24 20:32:27 +0200

    Remove unnecessary usings

--- BetterMatchmaking/Core/Core.cs
+++ BetterMatchmaking/Core/Core.cs
@@ -1,13 +1,5 @@
 ﻿using SharpPluginLoader.Core.Memory;
 using SharpPluginLoader.Core.Steam;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.ExceptionServices;
-using System.Security;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BetterMatchmaking;
 
